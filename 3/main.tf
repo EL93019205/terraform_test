a9ff29f81329f6fb75c0df517bcd6c247940e3cb
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-10 21:05:26 +0900

    モジュール
    http_serverにモジュールを作成しました。
    このとき、組み込み関数であるfileが使えなくなったので、直書きでshellコマンドを実行するようにしました。

--- 3/main.tf
+++ 3/main.tf
@@ -1,9 +1,9 @@
-provider "aws" {
-  region = "ap-northeast-1"
+module "web_server" {
+  source = "./http_server"
+  instance_type = "t3.micro"
 }
 
-resource "aws_instance" "example"{
-  ami = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
-  user_data = file("./user_data.sh")
+output "public_dns" {
+  value = module.web_server.public_dns
 }
+
