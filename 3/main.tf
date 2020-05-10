381fcfa4cc9dcd015e0395cddc84d5dae4226b7a
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-10 22:07:15 +0900

    モジュール失敗
    モジュールは失敗したのでまたの機会に
    (モジュールが失敗したというよりセキュリティグループとEC2インスタンスを同時に作ろうとするとうまく行かないかも？)

--- 3/main.tf
+++ 3/main.tf
@@ -1,13 +1,36 @@
-provider "aws"{
+provider "aws" {
   region = "ap-northeast-1"
 }
 
-module "web_server" {
-  source = "./http_server"
-  instance_type = "t3.micro"
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-output "public_dns" {
-  value = module.web_server.public_dns
+resource "aws_instance" "example" {
+  ami                     = "ami-0c3fd0f5d33134a76"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids  = [aws_security_group.example_ec2.id]
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+  EOF
 }
 
+output "example_public_dns" {
+  value = aws_instance.example.public_dns
+}
