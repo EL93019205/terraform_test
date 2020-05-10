7212ee2443c0b51d303fc99767840b4d4c3179e3
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-10 18:01:53 +0900

    変数
    変数を使用してインスタンスタイプを指定しました
    実行時に次のようにすることで、変数を書き換えることが可能です。
    terraform plan -var 'example_instance_type=t3.nano'

--- 3/main.tf
+++ 3/main.tf
@@ -1,5 +1,9 @@
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
 resource "aws_instance" "example"{
   ami = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
+  instance_type = var.example_instance_type
 }
 
