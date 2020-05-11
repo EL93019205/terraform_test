4a4329e044b0a40e1c906028c0fb76a6df9cf176
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-11 21:42:55 +0900

    信頼ポリシーの定義
    信頼ポリシーを定義しました。
    ["ec2.amazonaws.com"]は、EC2にのみ関連付けできるという意味です。

--- handson/main.tf
+++ handson/main.tf
@@ -11,3 +11,13 @@ resource "aws_iam_policy" "example" {
   policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
 
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
