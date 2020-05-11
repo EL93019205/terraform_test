b35e60476e3d4ad3786f75a0d068d99dcd26a10b
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-11 21:46:14 +0900

    IAMロール
    IAMロールを作成します。
    信頼ポリシーとロール名を指定します。

--- handson/main.tf
+++ handson/main.tf
@@ -21,3 +21,9 @@ data "aws_iam_policy_document" "ec2_assume_role" {
     }
   }
 }
+
+resource "aws_iam_role" "example" {
+  name = "example"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
