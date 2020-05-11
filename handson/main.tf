b2622bb8e8067f5ac808cddb1d19439756a15458
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-11 21:48:54 +0900

    IAMポリシーのアタッチ
    IAMロールにIAMポリシーをアタッチします。
    IAMロールとIAMポリシーは、関連付けないと機能しないので注意！

--- handson/main.tf
+++ handson/main.tf
@@ -27,3 +27,8 @@ resource "aws_iam_role" "example" {
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
 }
 
+resource "aws_iam_role_policy_attachment" "example" {
+  role = aws_iam_role.example.name
+  policy_arn = aws_iam_policy.example.arn
+}
+
