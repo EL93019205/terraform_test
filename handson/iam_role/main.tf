dee93a9dd4803a6c44766852a6b5513e34dbb32d
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-11 21:59:12 +0900

    IAMロールのモジュール化準備2
    順番を入れ替えます
    あと、1つリソースが不要なので削除します

--- handson/iam_role/main.tf
+++ handson/iam_role/main.tf
@@ -1,14 +1,6 @@
-data "aws_iam_policy_document" "allow_describe_regions" {
-  statement{
-    effect = "Allow"
-    actions = ["ec2:DescribeRegions"]
-    resource=["*"]
-  }
-}
-
-resource "aws_iam_policy" "example" {
+resource "aws_iam_role" "example" {
   name = "example"
-  policy = data.aws_iam_policy_document.allow_describe_regions.json
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
 }
 
 data "aws_iam_policy_document" "ec2_assume_role" {
@@ -22,18 +14,15 @@ data "aws_iam_policy_document" "ec2_assume_role" {
   }
 }
 
-resource "aws_iam_role" "example" {
+resource "aws_iam_policy" "example" {
   name = "example"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
 
 resource "aws_iam_role_policy_attachment" "example" {
   role = aws_iam_role.example.name
   policy_arn = aws_iam_policy.example.arn
 }
-
-
-
 
 
 
