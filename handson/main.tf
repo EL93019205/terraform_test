65410936b7d171d718dfebfaa9da95c1af3a626a
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-12 22:36:37 +0900

    VPCの作成
    VPCを作成します
    CIDRブロックは10.0.0.0/16にします
    
    ・enable_dns_supportとは
    trueにすると、AWSのDNSサーバーによる名前解決を有効にします
    
    ・enable_dns_hostnamesとは
    trueにすると、VPC内のリソースにパブリックDNSホスト名を自動的に割り当てます
    
    ・VPCを作成するときの注意
    タグをつけないとAWS上で名前がつかないので見分けがつかなくなります
    そのため、タグをつけるようにします

--- handson/main.tf
+++ handson/main.tf
@@ -82,3 +82,13 @@ data "aws_iam_policy_document" "alb_log" {
     }
   }
 }
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "example"
+  }
+}
