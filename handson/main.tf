1cd73d5d011843eaeb80517c068d53eba7e012fc
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-12 21:52:52 +0900

    ログバケットの作成
    ALBのログ保存用です
    有効期限を設定し、180日経ったらログを削除するようにします

--- handson/main.tf
+++ handson/main.tf
@@ -53,3 +53,14 @@ resource "aws_s3_bucket" "public" {
   }
 }
 
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-pragmatic-terraform"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
