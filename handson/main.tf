d6fcdd0a69d2b5f36b876856bcd1ca54e15c3ba2
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-12 23:50:43 +0900

    ルートテーブルの関連付け
    どのルートテーブルを使ってルーティングするかを、サブネット単位で判断する
    ・ルートテーブルの関連付けを行う時の注意
    関連付けを忘れるとデフォルトルートテーブルが自動的に使用されます
    アンチパターンのため、ルートテーブルの関連付けを忘れずに行います
    18.1章に詳細説明あり

--- handson/main.tf
+++ handson/main.tf
@@ -114,7 +114,10 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
 
 
 
