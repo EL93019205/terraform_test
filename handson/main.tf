bcffd2b1d697c86507ee45fbe2c3ecfcf788d051
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-12 16:27:58 +0900

    根本的に間違っていたので正しい形に修正
    ①IAMロール作成
    ②IAMポリシードキュメント作成
    ③IAMポリシー作成
    ④IAMロールとポリシーのアタッチ
    ここまでをモジュール化
    使用する場合は、名称と、何に関連付けするかと、何ができるかのポリシードキュメントを設定するだけでよい

--- handson/main.tf
+++ handson/main.tf
@@ -2,21 +2,6 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-variable "identifier" {
-  default = "ec2.amazonaws.com"
-}
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = [var.identifier]
-    }
-  }
-}
-
 data "aws_iam_policy_document" "allow_describe_regions" {
   statement {
     effect = "Allow"
@@ -25,12 +10,10 @@ data "aws_iam_policy_document" "allow_describe_regions" {
   }
 }
 
-
 module "describe_regions_for_ec2" {
   source     = "./iam_role"
   name       = "describe-regions-for-ec2"
-  identifier = var.identifier
-  policy1     = data.aws_iam_policy_document.assume_role.json
-  policy2     = data.aws_iam_policy_document.allow_describe_regions.json
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
 
