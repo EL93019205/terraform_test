e5cd9c89054ff210bfffbf3096dc396025d0d0d1
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-13 21:16:38 +0900

    NATゲートウェイのマルチAZ化
    パブリックサブネットについているNATゲートウェイをマルチAZ化します

--- handson/main.tf
+++ handson/main.tf
@@ -147,14 +147,25 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
 }
 
-resource "aws_eip" "nat_gateway" {
+resource "aws_eip" "nat_gateway_0" {
   vpc = true
   depends_on = [aws_internet_gateway.example]
 }
 
-resource "aws_nat_gateway" "example" {
-  allocation_id = aws_eip.nat_gateway.id
-  subnet_id = aws_subnet.public.id
+resource "aws_eip" "nat_gateway_1" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
+  subnet_id = aws_subnet.public_0.id
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id = aws_subnet.public_1.id
   depends_on = [aws_internet_gateway.example]
 }
 
