505945ae638485a772ec7265d1b10abfdc1284a5
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-13 22:04:36 +0900

    プライベートルートテーブルのAZ化
    プライベートルートテーブルをAZ化します

--- handson/main.tf
+++ handson/main.tf
@@ -153,9 +153,14 @@ resource "aws_route_table" "private_1" {
   vpc_id = aws_vpc.example.id
 }
 
-resource "aws_route_table_association" "private" {
-  subnet_id = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table_association" "private_0" {
+  subnet_id = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
 }
 
 resource "aws_eip" "nat_gateway_0" {
