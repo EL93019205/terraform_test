bf5ab918c70bdc16004600fe5819c942bb5b1f9c
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-13 21:03:15 +0900

    パブリックサブネットのマルチAZ化
    パブリックサブネットを複製します

--- handson/main.tf
+++ handson/main.tf
@@ -93,11 +93,18 @@ resource "aws_vpc" "example" {
   }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_0" {
   vpc_id = aws_vpc.example.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1c"
 }
 
 resource "aws_internet_gateway" "example" {
