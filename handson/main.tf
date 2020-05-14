473d0c0c892b370cece019678bf2da7c3cd50921
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-14 22:15:05 +0900

    ALBを追加します
    ・load_balancer_type
    "application"を設定するとALBになります
    "network"を設定するとNLBになります
    
    ・internal
    LBがVPC内部向けなのかインターネット向けなのかを指定します
    trueだとVPC内部向け
    falseだとインターネット向けになります
    
    ・idle_timeout
    タイムアウト時間を秒で設定します
    
    ・enable_deletion_protection
    trueにすると削除保護が有効になります
    本番環境で誤って削除しないためにはtrueにしておきます
    
    ・access_logs
    バケット名を指定するとアクセスログの保存が有効になります

--- handson/main.tf
+++ handson/main.tf
@@ -241,3 +241,29 @@ module "example_sg" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
+resource "aws_lb" "example" {
+  name = "example"
+  load_balancer_type = "application"
+  internal = false
+  idle_timeout = 60
+  enable_deletion_protection = true
+
+  subnets = [
+    aws_subnet.public_0.id,
+    aws_subnet.public_1.id,
+  ]
+
+  access_logs {
+    bucket = aws_s3_bucket.alb_log.id
+    enabled = true
+  }
+
+  security_groups = [
+    module.http_sg.security_group_id,
+    module.https_sg.security_group_id,
+    module.http_redirect_sg.security_group_id,
+  ]
+  tags = {
+    Name = "alb_example"
+  }
+}
