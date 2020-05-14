93a3b8d068832d21e9af70791c549b3451f21fad
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-14 22:26:41 +0900

    httpリスナーを作成します
    ・default_action
    実行するデフォルトのアクションを設定します
    forwardでリクエストを別のターゲットグループに転送します
    fixed-responseで固定のHTTPレスポンスを応答します
    redirectで別のURLにリダイレクトします

--- handson/main.tf
+++ handson/main.tf
@@ -296,3 +296,20 @@ module "http_redirect_sg" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "これはhttpです"
+      status_code = "200"
+    }
+  }
+}
+
+
