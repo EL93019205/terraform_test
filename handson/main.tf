05ee6c34e14383699b61ab1191e388ee3dbbaebc
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-14 23:03:19 +0900

    検証の待機を追加します
    これはリソースは作りません
    SSL証明書の検証が完了するまで待ってくれます

--- handson/main.tf
+++ handson/main.tf
@@ -351,3 +351,8 @@ resource "aws_route53_record" "example_certificate" {
   ttl = 60
 }
 
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn = aws_acm_certificate.example.arn
+  validation_record_fqdns = [aws_route53_record.example_certificate.fqdn]
+}
+
