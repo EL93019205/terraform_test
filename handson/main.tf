4224f86dee9eacb84ef7ff485d31e696ecef1bd6
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-16 21:22:48 +0900

    nginxのセキュリティグループを設定します
    portは80です

--- handson/main.tf
+++ handson/main.tf
@@ -467,3 +467,11 @@ resource "aws_ecs_service" "example" {
     ignore_changes = [task_definition]
   }
 }
+
+module "nginx_sg" {
+  source = "./security_group"
+  name = "nginx-sg"
+  vpc_id = aws_vpc.example.id
+  port = 80
+  cidr_blocks = [aws_vpc.example.cidr_block]
+}
