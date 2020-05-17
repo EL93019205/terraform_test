1502e7a7707868ac5a94e2ff25f9df85c462de8a
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-17 20:08:39 +0900

    DBパラメータグループを定義します
    ・name
    名前です
    ・family
    エンジン名とバージョンを合わせた値です
    ・parameter
    nameとvalueをまとめて変更します
    今回の場合、データベースの文字コードとサーバーの文字コードをutf8mb4にします

--- handson/main.tf
+++ handson/main.tf
@@ -584,3 +584,18 @@ resource "aws_ssm_parameter" "db_password" {
     ignore_changes = [value]
   }
 }
+
+resource "aws_db_parameter_group" "example" {
+  name = "example"
+  family = "mysql5.7"
+
+  parameter {
+    name = "character_set_database"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name = "character_set_server"
+    value = "utf8mb4"
+  }
+}
