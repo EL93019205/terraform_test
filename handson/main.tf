2c4fba5727b462b9940da1462dc5545863b9f991
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-23 16:32:03 +0900

    GithubWebhookを定義します
    ・repository
    名前です
    ・configuration
    urlはcodepipeline_webhookのurlです
    secretはaws側の秘密鍵と同じものを指定します
    content_typeはjsonを指定します
    insecure_sslはSSL認証を行うかどうかですが、今回は不要なのでfalseを指定します
    ・events
    トリガーとなるイベントを設定します
    pushのみ設定したり、他にはpull_requestも指定できます

--- handson/main.tf
+++ handson/main.tf
@@ -910,3 +910,15 @@ provider "github" {
   token = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 }
 
+resource "github_repository_webhook" "example" {
+  repository = "example"
+
+  configuration {
+    url = aws_codepipeline_webhook.example.url
+    secret = "VeryRandomStringMoreThan20Byte!"
+    content_type = "json"
+    insecure_ssl = false
+  }
+
+  events = ["push"]
+}
