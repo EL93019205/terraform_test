39ef8a53d7f0dd83195bca4c4602262f28b981e9
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-05-31 14:50:33 +0900

    KinesisDataFirehoseの配信ストリームが使用するIAMを作成します

--- handson/main.tf
+++ handson/main.tf
@@ -1028,3 +1028,30 @@ resource "aws_s3_bucket" "cloudwatch_logs" {
   }
   force_destroy = true
 }
+
+data "aws_iam_policy_document" "kinesis_data_firehose" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}",
+      "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}/*",
+    ]
+  }
+}
+
+module "kinesis_data_firehose_role" {
+  source = "./iam_role"
+  name = "kinesis-data-firehose"
+  identifier = "firehose.amazonaws.com"
+  policy = data.aws_iam_policy_document.kinesis_data_firehose.json
+}
